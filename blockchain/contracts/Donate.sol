fdb437906a0300022a6e2b46e26151ed679c1aa8
greatnessmensah <ephraimmensah99@gmail.com>
2023-08-10 12:06:34 +0000

    pseudocode

---
+++ blockchain/contracts/Donate.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+contract Donate {
+
+    // Get all organisations
+    function getOrganisations() public{}
+
+    // Send tokens
+    function makeDonation(uint amount, address add) public {}
+
+    // Track tokens received
+    function trackTokensReceived() public {}
+
+    // Withdraw tokens
+    function withdrawTokens() public {}
+
+    // Get all received tokens
+    function receivedTokens() public {}
+
+    // Get all withdrawn tokens
+    function withdrawnTokens() public {}
+
+    // Get donation link
+    function getDonationLink() public {}
+}
