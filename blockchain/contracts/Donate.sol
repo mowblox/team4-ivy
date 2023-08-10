7dbf393fb6b6e7e2867276635f2a1b67f1e16eb6
greatnessmensah <ephraimmensah99@gmail.com>
2023-08-10 13:25:45 +0000

    Revert "Revert "milestone 2.1 completed""
    This reverts commit 63cd2f93f4f9741f84a8e197ca96c9b16f5b85c2.

--- blockchain/contracts/Donate.sol
+++ blockchain/contracts/Donate.sol
@@ -2,9 +2,56 @@
 pragma solidity 0.8.19;
 
 contract Donate {
+    // create organization
+    address public owner;
+    uint public targetAmount;
+    uint public currentAmount;
 
-    // Get all organisations
-    function getOrganisations() public{}
+    constructor(uint _targetAmount) {
+        owner = msg.sender;
+        targetAmount = _targetAmount;
+    }
+    
+    struct Organization {
+        address creator;
+        string name;
+        string about;
+        uint goalAmount;
+        uint currentAmount;
+        uint createdTimestamp;
+    }
+
+    Organization[] public organizations;
+
+    function createOrganization(string memory _name, string memory _about, uint _goalAmount) public {
+        Organization memory newOrganization = Organization({
+            creator: owner,
+            name: _name,
+            about: _about,
+            goalAmount: _goalAmount,
+            currentAmount: 0,
+            createdTimestamp: block.timestamp
+        });
+        organizations.push(newOrganization);
+    }
+
+    
+    // get organisations count
+    function getOrganiationsCount() public view returns (uint) {
+        return organizations.length;
+    }
+
+    // get single organisation
+    function getOrganization(uint index) public view returns (Organization memory) {
+        require(index < organizations.length, "Invalid index");
+        Organization memory org = organizations[index];
+        return (org);
+    }
+
+   // Get all organisations
+    function getAllOrganizations() public view returns (Organization[] memory) {
+        return organizations;
+    }
 
     // Send tokens
     function makeDonation(uint amount, address add) public {}
