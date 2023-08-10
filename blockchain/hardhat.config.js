5de2a46a8c126891aad6a86170a4e2f37514328c
prkvw <neeqash@gmail.com>
2023-08-10 14:38:31 +0000

    Deploy env

--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -1,6 +1,19 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
+import HardhatUserConfig from 'hardhat/config';
+import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const HardhatUserConfig = {
   solidity: "0.8.19",
+  networks: {
+    mumbai: {
+      url: 'https://rpc-mumbai.maticvigil.com',
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  }
 };
+
+export default config;
